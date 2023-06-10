a2fe91f1ea31067226428b9a08ca3674d765edf3
Quan Teng Foong <e0564970@u.nus.edu>
2023-06-10 23:06:45 +0800

    add EquatableTests

--- CSPSolver/Sources/CSPSolver/Extensions/Equatable.swift
+++ CSPSolver/Sources/CSPSolver/Extensions/Equatable.swift
@@ -1,12 +1,12 @@
 extension Equatable {
     func isEqual(_ other: any Equatable) -> Bool {
         guard let other = other as? Self else {
-            return other.isExactlyEqual(self)
+            return other.inverseIsEqual(self)
         }
         return self == other
     }
 
-    private func isExactlyEqual(_ other: any Equatable) -> Bool {
+    private func inverseIsEqual(_ other: any Equatable) -> Bool {
         guard let other = other as? Self else {
             return false
         }
---
+++ CSPSolver/Tests/CSPSolverTests/ExtensionsTests/EquatableTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import CSPSolver
+
+final class EquatableTests: XCTestCase {
+    func testIsEqual_completelyDifferentTypes_returnsFalse() {
+        let equatableA = 1
+        let equatableB = "a"
+
+        XCTAssertFalse(equatableA.isEqual(equatableB))
+        XCTAssertFalse(equatableB.isEqual(equatableA))
+    }
+
+    func testIsEqual_differentTypesButCastable_returnsTrue() {
+        let equatableA: any Equatable = 1.0
+        let equatableB: Double = 1.0
+
+        XCTAssertTrue(equatableA.isEqual(equatableB))
+        XCTAssertTrue(equatableB.isEqual(equatableA))
+    }
+}
