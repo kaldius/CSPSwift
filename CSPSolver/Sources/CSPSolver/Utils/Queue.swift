befedced08bca90dae5541adb1a4ab8914384d6f
Quan Teng Foong <e0564970@u.nus.edu>
2023-06-16 20:40:41 +0800

    change to Khang's more efficient queue

--- CSPSolver/Sources/CSPSolver/Utils/Queue.swift
+++ CSPSolver/Sources/CSPSolver/Utils/Queue.swift
@@ -1,62 +1,121 @@
 /**
- A generic `Queue` class whose elements are first-in, first-out.
+ * Queue implemented with a singly-linked list.
+ *
+ * Offers O(1) adding to back and removing from front.
  */
-public struct Queue<T> {
-    private var queueArray = [T]()
+struct Queue<T> {
+    class Node<T> {
+        let value: T
+        var next: Node?
 
-    init(from array: [T] = [T]()) {
-        self.queueArray = array
-    }
+        init(value: T, next: Node? = nil) {
+            self.value = value
+            self.next = next
+        }
 
-    /// Adds an element to the tail of the queue.
-    /// - Parameter item: The element to be added to the queue
-    mutating func enqueue(_ item: T) {
-        queueArray.append(item)
-    }
-
-    /// Adds every element an an input array to the tail of the queue in order.
-    /// - Parameter array: The array of elements to be added to the queue
-    mutating func enqueueAll(in array: [T]) {
-        array.forEach({ queueArray.append($0) })
-    }
-
-    /// Removes an element from the head of the queue and return it.
-    /// - Returns: item at the head of the queue, nil if the queue is empty
-    mutating func dequeue() -> T? {
-        if self.isEmpty {
-            return nil
-        } else {
-            // TODO: this part is O(n). TOO SLOW! optimize with 2 stacks method?
-            return queueArray.removeFirst()
+        convenience init(value: T) {
+            self.init(value: value, next: nil)
         }
     }
 
-    /// Returns, but does not remove, the element at the head of the queue.
-    /// - Returns: item at the head of the queue
+    private var front: Node<T>?
+    private var back: Node<T>?
+    private(set) var count: Int
+    public var isEmpty: Bool { front == nil }
+
+    init(from array: [T] = [T]()) {
+        self.count = 0
+        array.forEach({ self.enqueue($0) })
+    }
+
+    /**
+     * Adds a new value to the queue
+     */
+    public mutating func enqueue(_ value: T) {
+        let fresh = Node(value: value)
+        if back != nil {
+            back?.next = fresh
+        }
+        back = fresh
+        if front == nil {
+            front = back
+        }
+        count += 1
+    }
+
+    /**
+     * Adds every element an an input array to the tail of the queue in order.
+     */
+    mutating func enqueueAll(in array: [T]) {
+        array.forEach({ self.enqueue($0) })
+    }
+
+    /**
+     * Retrieves the value at the front of the queue
+     */
+    @discardableResult
+    public mutating func dequeue() -> T? {
+        defer {
+            if let next = front?.next {
+                front = next
+            } else {
+                (front, back) = (nil, nil)
+            }
+            count = max(0, count - 1)
+        }
+        return front?.value
+    }
+
+    /**
+     * Returns, but does not remove, the element at the head of the queue.
+     */
     func peek() -> T? {
-        queueArray.first
+        front?.value
     }
 
-    /// The number of elements currently in the queue.
-    var count: Int {
-        queueArray.count
-    }
-
-    /// Whether the queue is empty.
-    var isEmpty: Bool {
-        queueArray.isEmpty
-    }
-
-    /// Removes all elements in the queue.
+    /**
+     * Removes all elements in the queue.
+     */
     mutating func removeAll() {
-        queueArray.removeAll()
+        (front, back) = (nil, nil)
     }
 
-    /// Returns an array of the elements in their respective dequeue order, i.e.
-    /// first element in the array is the first element to be dequeued.
-    /// - Returns: array of elements in their respective dequeue order
+    /**
+     * Returns an array of the elements in their respective dequeue order, i.e.
+     * first element in the array is the first element to be dequeued.
+     * - Returns: array of elements in their respective dequeue order
+     */
     func toArray() -> [T] {
-        queueArray
+        var outputArr = [T]()
+        var ptr = front
+        while true {
+            guard let element = ptr?.value else {
+                break
+            }
+            outputArr.append(element)
+            ptr = ptr?.next
+        }
+        return outputArr
+    }
+}
+
+// Display implementation for a single node
+extension Queue.Node: CustomStringConvertible {
+    public var description: String {
+        if let next = next {
+            return String(describing: next) + " <- " + "\(value)"
+        }
+        return "\(value)"
+    }
+}
+
+// Display implementation for the queue
+extension Queue: CustomStringConvertible {
+    public var description: String {
+        if let front = front {
+            return "[" + front.description + "]"
+        }
+        return "[]"
     }
 }
 
